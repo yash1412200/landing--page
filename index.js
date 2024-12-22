c88e92d127b272134ee77dceb707558ee65daf7f
yash <yashshembe14@gmail.com>
2024-12-22 20:24:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+
+var tl=gsap.timeline({
+    repeat:-1
+});
+
+tl
+.to(".imagecontainer",{
+    ease:Expo.easeInOut,
+    width:"100%",
+    
+    stagger:2,
+    top:0
+},'a')
+
+.to(".text h1",{
+
+    ease :Expo.easeInOut,
+   
+    stagger:2,
+    top:0
+},'a')
+
+.to(".text h1",{
+    delay:2,
+    ease :Expo.easeInOut,
+   
+    stagger:2,
+    top:"-100%"
+},'a')
